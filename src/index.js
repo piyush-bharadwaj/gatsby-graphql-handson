0464fb6b03bf26f60b2165ea5ba3966e0295f75c
Piyush Bharadwaj <piyush.b7@gmail.com>
2020-09-18 20:07:41 +0530

    fix console error:modified sample type and resolver

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("Inside index.js");
+import { GraphQLServer } from 'graphql-yoga';
+
+const typeDefs = `
+  type Query {
+    hello: String!
+  }`;
+
+const resolvers = {
+    Query: {
+        hello(){
+            return 'This is first query!';
+        }
+    }
+};
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+
+server.start(() => console.log('Server is running on localhost:4000'));
